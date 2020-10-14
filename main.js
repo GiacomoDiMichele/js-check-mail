d965d84c6808e81a1dffc18a8dfd575abe2b3b92
Giacomo <giacomo.1936@gmail.com>
2020-10-14 20:39:39 +0200

    impostato array

---
+++ main.js
@@ -0,0 +1,14 @@
+
+var email = ['giacomo.1936@gmail.com', 'giacomo.ur88@gmail.com', 'giacomo.ur1936@gmail.com'];
+
+var email_trovata = false;
+
+for (var i = 0; i < email.length; i++) {
+    console.log(email[i]);
+
+    var elementArray = email[i];
+
+    if (ele) {
+
+    }
+}
