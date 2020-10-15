e4e42353ffabfac98d0178270c76cd4e3e32dc7c
Giacomo <giacomo.1936@gmail.com>
2020-10-15 10:34:05 +0200

    modifica algoritmo

--- main.js
+++ main.js
@@ -2,16 +2,21 @@
 var email = ['giacomo.1936@gmail.com', 'giacomo.ur88@gmail.com', 'giacomo.ur1936@gmail.com'];
 
 var elementArray = prompt ("qual è la tua mail")
+console.log("la tua mail è " + elementArray);
+
+var email_trovata = false;
 
 for (var i = 0; i < email.length; i++) {
 
-    var email = email[i]
+    var email_corrente = email[i]
 
-    if (elementArray == email) {
-        console.log("email corretta");
+    if (elementArray == email_corrente) {
+        var email_trovata = true;
     }
 }
 
-if (elementArray != email) {
-    console.log("email non corretta");
+if (email_trovata == true) {
+    console.log("ci sei");
+} else {
+    console.log("non ci sei");
 }
